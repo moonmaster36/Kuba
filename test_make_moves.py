24fdd57fa2cc8416c8650845b9a23971acdb3d71
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-06-14 13:52:07 -0500

    Added move_right testing functions

--- test_make_moves.py
+++ test_make_moves.py
@@ -1,4 +1,37 @@
 import unittest
+from Board import *
 
 class TestMakeMove(unittest.TestCase):
-    pass
+    def test_move_right(self):
+        pass
+        # game = Kuba(('p1', 'W'), ('p2', 'B'))
+        # game.setupBoard()
+        # game.showBoard()
+        #
+        # # Implementing right move.
+        # print(f"1. make_move = {game.make_move('p1', (1, 0), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        # print(F'current_turn = {game.current_turn}')
+        #
+        # print(f"2. make_move = {game.make_move('p1', (1, 1), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"3. make_move = {game.make_move('p1', (1, 2), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"4. make_move = {game.make_move('p1', (1, 3), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"5. make_move = {game.make_move('p1', (1, 4), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"6. make_move = {game.make_move('p1', (1, 5), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # game.showGame()
