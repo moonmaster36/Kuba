0bcf686ec86fd19169cd8b891dbbd38cfbc8286b
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-06-13 22:51:55 -0500

    Just moved shift_right into make_move. Initial testing

--- practice_right.py
+++ practice_right.py
@@ -1,15 +1,27 @@
 from collections import deque
 import copy
 
-def shift_right(row, start, end):
-    # temp = start[0]
-    # start[0] = ' '
-    if end <= start:
-        return  False
+
+def get_marble(col, board):
+    return board[col]
+
+
+def shift_right(row, start):
     row_copy = copy.deepcopy(row)
+    # Find the end
+    end = start
+    while end < len(row_copy) and row_copy[end] != ' ':
+        end += 1
+    print(F'(start, end) = ({start, end})')
+    if end <= start:
+        return False
     print(f'before: {row_copy}')
-    for i in range(end + 1, start, -1):
-        print(i, row_copy)
+
+    if end == len(row_copy):
+        end -= 2
+
+    for i in range(end, start, -1):
+        print(i)
         row_copy[i] = row_copy[i - 1]
     row_copy[start] = ' '
     print(f'after: {row_copy}')
@@ -18,8 +30,6 @@ def shift_right(row, start, end):
 
 
 if __name__ == '__main__':
-    cur_row = ['W', 'W', 'R', 'R', 'R', 'B', 'B']
-    start = 0
-    end = 1
-
-    print(shift_right(cur_row, start, end))
+    # cur_row = ['W', 'W', 'R', 'R', 'R', 'B', ' ']
+    cur_row = ['W', 'W', ' ', ' ', ' ', 'B', 'B']
+    print(shift_right(cur_row, 0))
