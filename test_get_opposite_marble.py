10f5d00e653308a2612951b90a847a1b1afb47d8
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-06-13 19:54:58 -0500

    get_opposite_marble testing suite

--- test_get_opposite_marble.py
+++ test_get_opposite_marble.py
@@ -11,6 +11,9 @@ class TestValidateMove(unittest.TestCase):
         """
         test_game = Kuba(('p1', 'W'), ('p2', 'B'))
         test_game.setupBoard()
+        test_board = test_game.get_board()
+        test_board[0][2] = 'R'
+        test_board[2][0] = 'R'
         opposite_marble = test_game.get_opposite_marble((0, 1), 'B')
         self.assertIsNone(opposite_marble)
 
@@ -19,21 +22,49 @@ class TestValidateMove(unittest.TestCase):
         Expect left coords
         (row, col  - 1)
         """
-        pass
+        test_game = Kuba(('p1', 'W'), ('p2', 'B'))
+        test_game.setupBoard()
+        test_board = test_game.get_board()
+        test_board[0][2] = 'R'
+        test_board[2][0] = 'R'
+        opposite_marble = test_game.get_opposite_marble((0, 1), 'R')
+        self.assertEqual(opposite_marble, 'W')
 
     def test_forward_move(self):
         """
         Expect backward coords
         (row + 1, col)
         """
-        pass
+        test_game = Kuba(('p1', 'W'), ('p2', 'B'))
+        test_game.setupBoard()
+        test_board = test_game.get_board()
+        test_board[0][2] = 'R'
+        test_board[2][0] = 'R'
+        opposite_marble = test_game.get_opposite_marble((0, 1), 'F')
+        self.assertEqual(opposite_marble, 'W')
 
     def test_left_move(self):
         """
         Expect right coords
         (row + 1, col)
         """
-        pass
+        test_game = Kuba(('p1', 'W'), ('p2', 'B'))
+        test_game.setupBoard()
+        test_board = test_game.get_board()
+        test_board[0][2] = 'R'
+        test_board[2][0] = 'R'
+        opposite_marble = test_game.get_opposite_marble((0, 1), 'L')
+        self.assertEqual(opposite_marble, 'R')
+
+    def test_empty_space(self):
+        test_game = Kuba(('p1', 'W'), ('p2', 'B'))
+        test_game.setupBoard()
+        test_board = test_game.get_board()
+        test_board[0][1] = ' '
+        test_board[2][1] = 'W'
+        opposite_marble = test_game.get_opposite_marble((1, 1), 'B')
+        self.assertEqual(opposite_marble, ' ')
+
 
 
 if __name__ == '__main__':
