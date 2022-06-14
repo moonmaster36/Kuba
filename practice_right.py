d573cb9166821353d17cfe2c8e83b62b0f6428a8
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-06-13 22:37:52 -0500

    90% finished with practice_Right

--- practice_right.py
+++ practice_right.py
@@ -4,19 +4,22 @@ import copy
 def shift_right(row, start, end):
     # temp = start[0]
     # start[0] = ' '
+    if end <= start:
+        return  False
     row_copy = copy.deepcopy(row)
     print(f'before: {row_copy}')
-    for i in range(start + 1, end + 1):
-        print(i)
-        temp = row_copy[i]
-        next = row_copy[i + 1]
+    for i in range(end + 1, start, -1):
+        print(i, row_copy)
         row_copy[i] = row_copy[i - 1]
+    row_copy[start] = ' '
     print(f'after: {row_copy}')
+
+    return True
 
 
 if __name__ == '__main__':
-    cur_row = ['W', 'W', 'B', ' ', ' ', 'B', 'B']
+    cur_row = ['W', 'W', 'R', 'R', 'R', 'B', 'B']
     start = 0
-    end = 3 
+    end = 1
 
     print(shift_right(cur_row, start, end))
