d2e0221571874c5b3087776b298ff417ffe680af
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-06-14 16:20:32 -0500

    Added test framework for move left

--- test_make_moves.py
+++ test_make_moves.py
@@ -35,3 +35,49 @@ class TestMakeMove(unittest.TestCase):
         # game.set_turn('p1')
         #
         # game.showGame()
+
+    def test_move_left(self):
+        pass
+
+    # game = Kuba(('p1', 'W'), ('p2', 'B'))
+    # game.setupBoard()
+    # game.showBoard()
+    #
+    # # Implementing left move. Moving white on (5,6) to left
+    # print(f"1.make_move = {game.make_move('p1', (5, 6), 'L')}")
+    # game.showBoard()
+    # game.set_turn('p1')
+    #
+    # print(f"2.make_move = {game.make_move('p1', (5, 5), 'L')}")
+    # game.showBoard()
+    # game.set_turn('p1')
+    #
+    # print(f"3.make_move = {game.make_move('p1', (5, 4), 'L')}")
+    # game.showBoard()
+    # game.set_turn('p1')
+    #
+    # print(f"4.make_move = {game.make_move('p1', (5, 3), 'L')}")
+    # game.showBoard()
+    # game.set_turn('p1')
+    #
+    # print(f"5.make_move = {game.make_move('p1', (5, 2), 'L')}")
+    # game.showBoard()
+    # game.set_turn('p1')
+    #
+    # # 6 & 7 try to push our own marble off
+    # print(f"6.make_move = {game.make_move('p1', (5, 1), 'L')}")
+    # game.showBoard()
+    # game.set_turn('p1')
+    #
+    # print(f"7.make_move = {game.make_move('p1', (5, 0), 'L')}")
+    # game.showBoard()
+    # game.set_turn('p1')
+    #
+    # game.showGame()
+
+
+    def test_move_backward(self):
+        pass
+
+    def test_move_forward(self):
+        pass
