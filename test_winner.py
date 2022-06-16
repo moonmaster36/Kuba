2d37faa3284bf1348063b16bf3338ea0805639d1
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-06-16 09:28:13 -0500

    Tests for detecting if game is won

---
+++ test_winner.py
@@ -0,0 +1,124 @@
+import unittest
+from Board import *
+
+
+class TestWinnerMakeMove(unittest.TestCase):
+    def test_p1_white_wins(self):
+        # game = Kuba(('p1', 'W'), ('p2', 'B'))
+        # game.setupBoard()
+        # game.showBoard()
+        #
+        # # Implementing right move. 1
+        # print(f"1. make_move = {game.make_move('p1', (1, 0), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        # print(F'current_turn = {game.current_turn}')
+        #
+        # print(f"2. make_move = {game.make_move('p1', (1, 1), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"3. make_move = {game.make_move('p1', (1, 2), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"4. make_move = {game.make_move('p1', (1, 3), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"5. make_move = {game.make_move('p1', (1, 4), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"6. make_move = {game.make_move('p1', (1, 5), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # # Implementing right move. 2
+        # print(f"1. make_move = {game.make_move('p1', (0, 0), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        # print(F'current_turn = {game.current_turn}')
+        #
+        # print(f"2. make_move = {game.make_move('p1', (0, 1), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"3. make_move = {game.make_move('p1', (0, 2), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"4. make_move = {game.make_move('p1', (0, 3), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"5. make_move = {game.make_move('p1', (0, 4), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"6. make_move = {game.make_move('p1', (0, 5), 'R')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # # # Implementing left move. Moving white on (5,6) to left
+        # print(f"1.make_move = {game.make_move('p1', (5, 6), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"2.make_move = {game.make_move('p1', (5, 5), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"3.make_move = {game.make_move('p1', (5, 4), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"4.make_move = {game.make_move('p1', (5, 3), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"5.make_move = {game.make_move('p1', (5, 2), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # # 6 & 7 try to push our own marble off
+        # game.make_move('p1', (5, 1), 'L')
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"7.make_move = {game.make_move('p1', (5, 0), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # # # Implementing left move. Moving white on (5,6) to left
+        # print(f"1.make_move = {game.make_move('p1', (6, 6), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"2.make_move = {game.make_move('p1', (6, 5), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"3.make_move = {game.make_move('p1', (6, 4), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"4.make_move = {game.make_move('p1', (6, 3), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"5.make_move = {game.make_move('p1', (6, 2), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # # 6 & 7 try to push our own marble off
+        # print(f"6.make_move = {game.make_move('p1', (6, 1), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # print(f"7.make_move = {game.make_move('p1', (6, 0), 'L')}")
+        # game.showBoard()
+        # game.set_turn('p1')
+        #
+        # game.showGame()
+        pass
