8388e0366389b985653b6d8a80969f4904c91f9d
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-06-17 14:41:08 -0500

    Updated tests

--- tests/test_ko_rule.py
+++ tests/test_ko_rule.py
@@ -24,4 +24,29 @@ class TestMakeMove(unittest.TestCase):
         pass
 
     def test_vertical_ko_rule(self):
+        # game = Board(('p1', 'W'), ('p2', 'B'))
+        # p1 = game.get_player('p1')
+        # p2 = game.get_player('p2')
+        # game.setupBoard()
+        #
+        # # Implementing Ko Rule Testing.
+        # game.make_move('p1', (0, 0), 'B')
+        # game.make_move('p2', (6, 0), 'F')
+        #
+        # game.make_move('p1', (1, 0), 'B')
+        # # game.showBoard()
+        # game.make_move('p2', (5, 0), 'F')
+        # game.showBoard()
+        #
+        # # Violation
+        # game.make_move('p1', (1, 0), 'B')
+        # game.showBoard()
+        # game.make_move('p1', (0, 1), 'B')
+        # game.showBoard()
+        #
+        # p2.showBoardStateAfterMove()
+        # p1.showBoardStateAfterMove()
+        # game.make_move('p2', (4, 0), 'F')
+        # game.showBoard()
         pass
+
