cce7318051b8745fe348c22fbcf5272fecb44548
moonmaster36 <76980875+moonmaster36@users.noreply.github.com>
2022-06-21 13:36:37 -0500

    initial commit

---
+++ KubaGame/tkinter_test1.py
@@ -0,0 +1,45 @@
+# Assets: https://techwithtim.net/wp-content/uploads/2020/09/assets.zip
+import pygame
+from KubaGame.kuba import Kuba
+from KubaGame.scoreboard import ScoreBoard
+from KubaGame.constants import RED, WHITE, BLACK, BLUE, GREY, SQUARE_SIZE, ROWS, COLS, WIDTH, HEIGHT
+
+FPS = 60
+
+KUBA_WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Kuba')
+programIcon = pygame.image.load('assets/marbles.png')
+pygame.display.set_icon(programIcon)
+
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    kuba = Kuba(KUBA_WINDOW)
+
+    while run:
+        clock.tick(FPS)
+
+        if kuba.get_winner():
+            print(f'Winner: {kuba.get_winner()}')
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                kuba.select(row, col, KUBA_WINDOW)
+        kuba.update_kuba_window()
+
+
+main()
